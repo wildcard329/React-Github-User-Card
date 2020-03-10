57478ce143cf34580222c3b1770983a8c8f31b67
wildcard329 <enkiduwildman@gmail.com>
2020-03-10 12:41:52 -0700

    initialized project, changed name to github user card

---
+++ gh_user_card/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import axios from 'axios';
+import './App.css';
+
+class App extends React.Component {
+  
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
+
+// Step one: class app extends react.component
+// Step two: put return inside of render()
+// Step three: set up componentdidmount

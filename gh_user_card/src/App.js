4f20af51cdc4fa36056104feae35a9227dec26f7
wildcard329 <enkiduwildman@gmail.com>
2020-03-10 13:33:20 -0700

    rendering user card

--- gh_user_card/src/App.js
+++ gh_user_card/src/App.js
@@ -3,11 +3,40 @@ import axios from 'axios';
 import './App.css';
 
 class App extends React.Component {
+  state = {
+    user: {},
+    followers: []
+  }
+  
+  componentDidMount() {
+    axios
+      .get('https://api.github.com/users/wildcard329')
+      .then(res => {
+        console.log(res)
+        this.setState({
+          user: res.data
+        })
+      })
+      .catch(err => console.log(err))
+
+      axios
+        .get('https://api.github.com/users/wildcard329/followers')
+        .then(res => {
+          console.log(res)
+          this.setState({
+            followers: res.data.login
+          })
+        })
+  }
   
   render() {
     return (
       <div className="App">
-
+        <h1>Github Users</h1>
+        <h3>{this.state.user.login}</h3>
+        <img src={this.state.user.avatar_url} />
+        <p>Followers: {this.state.user.followers}</p>
+        <p>Following: {this.state.user.following}</p>
       </div>
     );
   }
@@ -19,4 +48,9 @@ export default App;
 
 // Step one: class app extends react.component
 // Step two: put return inside of render()
-// Step three: set up componentdidmount
+// Step three: set up state
+// Step four: set up componentdidmount
+// Step five: use axios to connect to api, log results
+// Step six: set up state and return based on console results
+// note for step six: instead of directly using users.var, 
+// the variable needs to follow this.state
